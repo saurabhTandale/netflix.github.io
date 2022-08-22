b6c5431b0318c61d8a12202e3bd446ee28c0e477
Sau <110614172+saurabhTandale@users.noreply.github.com>
2022-08-22 17:51:01 +0530

    netflix.github.io

---
+++ js/script.js
@@ -0,0 +1,107 @@
+// question 1
+
+const targetDiv = document.getElementById('answer1');
+const btn = document.getElementById('question1');
+
+btn.onclick = function () {
+  if(targetDiv.style.display !== 'none') {
+    targetDiv.style.display = 'none';
+  }else {
+    targetDiv.style.display = 'block';
+  }
+};
+
+
+// Question 2 
+
+
+const questionSecond = document.getElementById('answer2');
+const questionButton = document.getElementById('question2');
+
+questionButton.onclick = function () {
+  if(questionSecond.style.display !== 'none') {
+    questionSecond.style.display = 'none';
+  }else {
+    questionSecond.style.display = 'block';
+  }
+};
+
+
+// question 3 
+
+
+const questionThird = document.getElementById('answer3');
+const thirdButton = document.getElementById('question3');
+
+
+thirdButton.onclick = function () {
+  if(questionThird.style.display !== 'none'){
+    questionThird.style.display = 'none';
+  }else {
+    questionThird.style.display = 'block';
+  }
+};
+
+
+// question 4 
+
+const questionFourth = document.getElementById('answer4');
+const fourthButton = document.getElementById('question4');
+
+fourthButton.onclick = function () {
+  if(questionFourth.style.display !== 'none') {
+    questionFourth.style.display = 'none'
+  }else {
+    questionFourth.style.display = 'block';
+  }
+};
+
+
+
+// question 5 
+
+
+const questionFifth = document.getElementById('answer5');
+const fifthButton = document.getElementById('question5');
+
+fifthButton.onclick = function () {
+  if(questionFifth.style.display !== 'none'){
+    questionFifth.style.display = 'none';
+  }else {
+    questionFifth.style.display = 'block';
+  }
+};
+
+// question 6 
+
+const questionSix = document.getElementById('answer6');
+const sixButton = document.getElementById('question6');
+
+sixButton.onclick = function () {
+  if(questionSix.style.display !== 'none'){
+    questionSix.style.display = 'none';
+  }else {
+    questionSix.style.display = 'block';
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
